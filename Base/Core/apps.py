aade8329f8db7c89caeb9bfa587ff81f47ebead8
Nifoozmemeir <julian.ers@hotmail.com>
2025-10-10 11:21:04 -0300

    Reestructuración TOTAL del proyecto, se acomodaron todas las apps a un diseño mejor, se agregó postgresql y se actualizaron las librerías

--- Base/Core/apps.py
+++ Base/Core/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppbaseConfig(AppConfig):
+class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'AppBase'
+    name = 'Base.Core'
